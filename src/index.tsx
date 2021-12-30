fc18948f4f66a0d4428c997363a0a728dadd7044
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2021-12-29 23:23:20 -0300

    Feito o seeds, colocado na tela e formatado valores

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer, Model } from 'miragejs';
+import { createServer, Model, Server } from 'miragejs';
 
 createServer({
 
   models: {
     transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-01 09:00:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1110,
+          createdAt: new Date('2021-02-14 09:49:00:00'),
+        },
+      ]
+    })
   },
 
   routes() {
